4f216637b21bf05ef1cf1b0efacc419de5a129a9
jdharris-gold <jdharris.gold@gmail.com>
2026-07-16 20:24:04 -0500

    Update wealth.js

--- js/wealth.js
+++ js/wealth.js
@@ -17,15 +17,17 @@ document.getElementById("saveWealth").addEventListener("click", async () => {
     const assets = Number(document.getElementById("assets").value);
     const debts = Number(document.getElementById("debts").value);
     const netWealth = assets - debts;
-    alert("bluebullSave() fired!"); //This alert fired successfully
+    alert("Fired 1!"); //This alert fired successfully
     document.getElementById("netWealth").innerText = netWealth; // This executed successfully
     console.log("Reached BEFORE second alert");
-    alert("Reached BEFORE second alert");
+    alert("Fired 2"); // This executed successfully
 
     console.log("Calling bluebullSave now...");
+     alert("Fired 3"); // This executed successfully
     
     try {
         await bluebullSave("wealth", { assets, debts, netWealth });
+        alert("Fired 4"); // This executed successfully
     } catch (err) {
         alert("ERROR: " + err.message);
         console.error(err);
